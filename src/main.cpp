4a1b08939b972eddd45c0fb8cb09c3b11b22aa26
Teeheene <diane.panganiban1010@gmail.com>
2025-11-02 21:40:43 +0800

    feat: implement menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,107 @@
 #include <iostream>
+#include <vector>
+#include <atomic>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+atomic<bool> running(true);
+
+/*
+ * tokenizes the input
+ *
+ * @param input - the string input of the user 
+ * @returns vector<string> - dynamic array of tokens 
+ * */
+std::vector<std::string> tokenizeInput(std::string input) {
+	std::vector<std::string> tokens;
+	std::string token{ "" };
+	
+	if(input.empty())
+		return {}; 
+
+	for(char ch : input) {
+		//if its not a space
+		if(!std::isspace(static_cast<unsigned char>(ch))) {
+			token += ch;
+		} else {
+			tokens.push_back(token);
+			token = "";
+		}
+	}
+	tokens.push_back(token);
+
+	return tokens;
+}
+
+class MainController {
+	string rawInput;
+	vector<string> cmd;
+	bool initialized;
+	
+public:
+	MainController() {
+		initialized = false;
+	}
+
+	void run() {
+		while(running) {
+			cout << "root:\\> ";
+			getline(cin, rawInput);
+			cmd = tokenizeInput(rawInput);
+
+			if(!initialized) {
+				if(cmd[0] == "initialize") {
+					cout << "initializing processor configuration..." << endl << endl;
+					//read and setup config
+					initialized = true;
+				} 
+				else if(cmd[0] == "exit"){
+					cout << "exiting program..." << endl << endl;
+					running = false;
+				}
+				else {
+					cout << "Please initialize first!" << endl << endl;
+				}
+			}
+
+			else if(initialized) {
+				if(cmd[0] == "screen") {
+					if(cmd.size() == 1) {
+						cout << "Missing argument after 'screen'" << endl << endl;
+					}
+					else if(cmd[1] == "-s") {
+						//clear contents
+						//create a new process
+						//"move" to process screen (possibly call a class for it)
+					} 
+					else if(cmd[1] == "-ls") {
+						//list all running processes
+					}
+				}
+				else if(cmd[0] == "scheduler-start") {
+					//activate the thread for generating processes in the background
+				}
+				else if(cmd[0] == "scheduler-stop") {
+					//stop generating dummy processes
+				}
+				else if(cmd[0] == "report-util") {
+					//generate CPU util report 
+				}
+				else if(cmd[0] == "exit") {
+					cout << "exiting program..." << endl << endl;
+					running = false;
+				} else {
+					cout << "Unknown command." << endl << endl;
+				}
+			}
+		}
+	}	
+};
 
 int main() {
-
+	MainController os;
+	os.run();
 }
